71367bf6d438dfbe74265599e4567d335e5d1c9e
ruyndang <119937802+ruyndang@users.noreply.github.com>
2023-02-27 20:21:47 +0700

    buổi 17

---
+++ Buổi 17 27-2-2023/practice2_array.js
@@ -0,0 +1,20 @@
+
+//  Bài 2: Hãy tạo ra một mảng  mới bằng cách cộng các phần tử tương ứng của mảng  cũ theo index tương ứng
+let array1 = ["M", "na", "i", "Ke"];
+let array2 = ["y", "me", "s", "lly"];
+// ['My', 'name', 'is', 'Kelly']
+
+ let arr1 = [array1[0],array2[0]];
+ let str1 = arr1.join("");
+ let arr2 = [array1[1],array2[1]];
+ let str2 = arr2.join("");
+ let arr3 = [array1[2],array2[2]];
+ let str3 = arr3.join("");
+ let arr4 = [array1[3],array2[3]];
+ let str4 = arr4.join("");
+ let array3 = [str1, str2, str3, str4];
+
+ 
+
+
+document.write(array3);
---
+++ Buổi 17 27-2-2023/practice3_array.js
@@ -0,0 +1,12 @@
+// Bài 3: Cho sẵn một mảng  chứa các số tự nhiên. 
+// Bình phương các phần tử trong mảng và lưu vào một mảng  mới.
+let array1 = [1, 2, 3, 4, 5, 6, 7];
+let array2 = [];
+let array3 = [];
+for (i=0; i <= array1.length; i ++){
+
+array2.push(array1[i]*array1[i]);
+array3 = 
+}
+
+// [1, 4, 9, 16, 25, 36, 49]
